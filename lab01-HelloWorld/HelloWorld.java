460093d47de92282b2a4dd23fd01a4c4598d2fec
iym219 <iym219@lehigh.edu>
2015-08-28 13:52:39 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+//By Isaak Melendez
+//Creating my first Java Program
+//need to define a class
+public class HelloWorld{
+    public static void main(String[] args) {
+//time to print 
+    System.out.println("####################");
+    System.out.println("####################");
+    System.out.println("#                  #");
+    System.out.println("#  Hello World     #");
+    System.out.println("#   From Isaak     #");
+    System.out.println("#                  #");
+    System.out.println("####################");
+    System.out.println("####################");
+    
+    //time for the test
+    }
+}
