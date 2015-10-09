517888161c0e636e499aa0c79372795b6d93ac8b
iym219 <iym219@lehigh.edu>
2015-10-09 14:19:54 +0000

    Isaak Melendez

---
+++ LecturesNotes/loops.java
@@ -0,0 +1,27 @@
+public class loops{
+    public static void main(String [] args){
+  //      double pie=0;
+//        for (int p=1; p<=5000; p=p+4){
+        //    pie= pie+(1/p)-(1/p+1);
+      //  }
+    //    System.out.println(4+pie);
+  //  }
+//}
+  //  for(int l=0;l<900;l++){
+//        for (int w=0; w<900; w++){
+          //  System.out.println("*");
+        //}
+      //  System.out.println(" ");
+    //}
+        int a,b;
+        System.out.println("Choose a 1-9 digit");
+        for(a=1;a<10;a++){
+            System.out.println(a+1);
+        }
+        for(b=1;b<10;b++){
+            int k=a*b;
+            System.out.println(k+" ");
+        }
+        System.out.println(" ");
+    }
+}
---
+++ lab07-NestedLoops/encrypted_x.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class encrypted_x{
+    public static void main(String [] args){
+        Scanner myScanner= new Scanner(System.in);
+        System.out.print("Choose your size for X. Only between 0-100. ");
+        int input=myScanner.nextInt();        
+        int j, i;
+        if(input<100 && input>0){
+            for (i=0;i<input;i++){
+                for(j=0;j<input;j++){
+                    if((input-i)==j+1){
+                        System.out.print(" ");
+                    }
+                    else if(i == j) {
+                        System.out.print(" ");
+                    }
+                    else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+        }
+        else{
+            System.exit(-1);
+        }
+    }
+}
