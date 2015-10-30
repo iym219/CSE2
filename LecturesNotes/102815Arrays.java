9ad7b7cbd6408e7e805d45917b3ac0dec2cbc234
iym219 <iym219@lehigh.edu>
2015-10-30 04:36:26 +0000

    Isaak Melendez

---
+++ LecturesNotes/102815Arrays.java
@@ -0,0 +1,8 @@
+public class 102815Arrays{
+    public static void main(String [] args){
+        int [] arr; //this is 5 he method for declaration
+        arr= new int [5];
+        arr[0]=5;//This results in showing number5 in the println
+        System.out.println("arr[0]= "+arr[0]);//This result in zero
+    }
+}
---
+++ lab10-Arrays/arrays.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class arrays{
+    public static void main (String [] args){
+        Scanner myScanner = new Scanner (System.in);
+        System.out.print("Enter the numbers of arrays that you want ");
+        int nArray=myScanner.nextInt();
+        int [] array = new int [nArray];
+        for(int x=0;x<nArray;x++){
+            array [x]=(int) (Math.random()*100);
+        }
+        for(int y=0;y<nArray;y++){
+            System.out.println(array[y]);
+        }
+        int sum=0;
+        for(int z=0;z<nArray;z++){
+            sum+=array[z];
+        }
+        double av=(double)sum/(double)nArray;
+        System.out.println("The total sum of all array numbers is "+sum+" The average with the array is "+av);
+        for(int w=0;w<nArray;w=0){
+            if(array[w]>=av){
+                System.out.println("The numbers greater than the average are "+array[w]);
+            }
+        return;
+        }
+    }
+}
